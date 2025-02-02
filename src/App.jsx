29400a7b45f8307db9c7b37adcf46ed7ab2b3dd5
MirzayevAlisher077 <MirzayevAlisher078@gmail.com>
2025-02-02 20:47:03 +0300

    just starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Hero from "./components/hero/Hero"
 
 function App() {
 
 
   return (
-    <div className="h-40 w-full bg-green-700">
-      <h1>Coders Cafe</h1>
-      <p>I will start doing it next week</p>
-
+    <div className=" overflow-x-hidden">
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <main className='bgImage'>
+        <section>
+            <div>
+                {/* navbar section */}
+                {/* hero section */}
+                <div className='grid grid-cols-1 md:grid-cols-2 '></div>
+            </div>
+        </section>
+    </main>
+  )
+}
+
+export default Hero
